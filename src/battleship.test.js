e66fb6c9da17a3684045a9c833cc7b44ca29929d
Cameron Brown <moontsukuyomi6@gmail.com>
2025-10-15 16:28:36 +0200

    test: add unit tests for hit method

--- src/battleship.test.js
+++ src/battleship.test.js
@@ -9,3 +9,16 @@ test("Ship class => length: 3, damage: 0, sunk: false", () => {
   const ship = new Ship(3);
   expect(ship).toEqual({ length: 3, damage: 0, sunk: false });
 });
+
+test("Hit method => length: 3, damage: 1, sunk: false", () => {
+  const ship = new Ship(3);
+  ship.hit();
+  expect(ship).toEqual({ length: 3, damage: 1, sunk: false });
+});
+
+test("Hit method => length: 3, damage: 2, sunk: false", () => {
+  const ship = new Ship(3);
+  ship.hit();
+  ship.hit();
+  expect(ship).toEqual({ length: 3, damage: 2, sunk: false });
+});
