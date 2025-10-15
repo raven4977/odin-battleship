c4152951ce402dacf85c55add4a26144679fa7bf
Cameron Brown <moontsukuyomi6@gmail.com>
2025-10-15 16:25:16 +0200

    feat: add Ship class to handle creating ship objects

---
+++ src/battleship.js
@@ -0,0 +1,21 @@
+class Ship {
+  constructor(length) {
+    this.length = length;
+    this.damage = 0;
+    this.sunk = false;
+  }
+}
+
+module.exports = {
+  Ship,
+};
+
+/* 
+Battleship Logic:
+1. 2 10x10 boards
+2.1 Both players must have 4 ships of length 1, 3 ships of length 2, 2 ships of length 3, 1 ship of length 4, phase 1 starts with players placing ships
+2.2 Ship placement must be at least 1 square apart
+3. Consider building a computer enemy
+4. Player 1 goes first, if player hits an enemy ship then players goes again, if player misses players swap turns, if player selects the same square return before anything happens
+5. Players win when all enemy ships are destroyed
+*/
