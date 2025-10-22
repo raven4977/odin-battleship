ff83ca0aff0a35bc222a0da0910aa20060a111f3
Cameron Brown <moontsukuyomi6@gmail.com>
2025-10-22 17:55:26 +0200

    feat: add placeShip function to place a ship on the board and currentShips and maxShips objects to handle tracking placed ships and ship limitations

--- src/scripts/battleship.js
+++ src/scripts/battleship.js
@@ -15,6 +15,38 @@ class Ship {
   }
 }
 
+const Gameboard = () => {
+  const board = new Array(100).fill(false);
+  const currentShips = {
+    length1: 0,
+    length2: 0,
+    length3: 0,
+    length4: 0,
+  };
+  const maxShips = {
+    length1: 4,
+    length2: 3,
+    length3: 2,
+    length4: 1,
+  };
+  const placeShip = (length, coordinates) => {
+    const isValid = validate(
+      length,
+      coordinates,
+      currentShips,
+      maxShips,
+      board
+    );
+    if (isValid) {
+      const ship = new Ship(length);
+      currentShips[`length${length}`]++;
+      coordinates.forEach((coordinate) => (board[coordinate] = ship));
+    }
+  };
+  return { board, currentShips, maxShips, placeShip };
+};
+
 module.exports = {
   Ship,
+  Gameboard,
 };
