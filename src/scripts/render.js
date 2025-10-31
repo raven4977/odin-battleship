0413aace4c87fa54d4519e2c376152f43d563b54
Cameron Brown <moontsukuyomi6@gmail.com>
2025-11-01 01:02:59 +0200

    feat: add a function to handle displaying attacks

--- src/scripts/render.js
+++ src/scripts/render.js
@@ -1,3 +1,5 @@
+import { events } from "./events.js";
+
 const render = (() => {
   const renderBoard = (player) => {
     const board = player.gameboard.board;
@@ -12,11 +14,38 @@ const render = (() => {
           div.textContent = square.length;
           div.classList.add("ship");
         }
+      } else {
+        events.computerBoardEvents(div, index, player);
       }
       boardElement.appendChild(div);
     });
   };
-  return { renderBoard };
+  const displayAttack = (attack, index, player) => {
+    const playerType = player.player ? "player" : "computer";
+    const boardElement = document.querySelector(`.${playerType}-board`);
+    if (!attack) {
+      boardElement.children[index].textContent = "X";
+      boardElement.children[index].classList.add("--miss");
+    } else if (attack.sunk) {
+      attack.coordinates.forEach((coor) => {
+        const cell = boardElement.children[coor];
+        const image = document.createElement("img");
+        const existingImages = cell.querySelectorAll("img");
+        existingImages.forEach((img) => img.remove());
+        image.classList.add("icon");
+        image.src = "./images/bullseye-solid-full.svg";
+        cell.appendChild(image);
+        cell.classList.add("--sunk");
+      });
+    } else {
+      const image = document.createElement("img");
+      image.classList.add("icon");
+      image.src = "./images/ship-solid-full.svg";
+      boardElement.children[index].appendChild(image);
+      boardElement.children[index].classList.add("--hit");
+    }
+  };
+  return { renderBoard, displayAttack };
 })();
 
 export { render };
