e0eeb8378a9f216179e784a7f2e7eaaec48de963
Cameron Brown <moontsukuyomi6@gmail.com>
2025-10-31 23:16:30 +0200

    feat: add isSunk method to calculate if a ship has been sunk

--- src/scripts/battleship.js
+++ src/scripts/battleship.js
@@ -5,9 +5,16 @@ class Ship {
     this.sunk = false;
     this.coordinates = [];
   }
+
   hit() {
     this.damage++;
+    this.isSunk();
     return this;
+  }
+
+  isSunk() {
+    if (this.damage === this.length) this.sunk = true;
+    return this.sunk;
   }
 }
 
