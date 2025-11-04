4c8c88bb33e292a72f3478f51db9634fea5614a3
Cameron Brown <moontsukuyomi6@gmail.com>
2025-11-04 04:35:24 +0200

    feat: add a function to allow users to drag and drop ships

--- src/scripts/events.js
+++ src/scripts/events.js
@@ -1,6 +1,6 @@
 import { playGame } from "./battleship.js";
 import { render } from "./render.js";
-import { updateDisplayMessage } from "./utility.js";
+import { updateDisplayMessage, validate } from "./utility.js";
 
 const events = (() => {
   const resultElement = document.querySelector(".display-result");
@@ -63,7 +63,99 @@ const events = (() => {
       }
     });
   };
-  return { startGameEvent, computerBoardEvents, shuffleButtonEvents };
+
+  const dragClickShips = (player, div, square, board) => {
+    let mouseIsDown = false;
+    let selectedShip = null;
+    div.addEventListener("mousedown", () => {
+      const gameState = playGame.getGameState();
+      if (gameState.gameStatus) return;
+      const playerBoard = player.gameboard.board;
+      const playerBoardSquares = document.querySelectorAll(
+        ".player-board .square"
+      );
+      mouseIsDown = true;
+      if (mouseIsDown && square) {
+        selectedShip = square;
+        playerBoardSquares.forEach((square) =>
+          square.classList.remove("selected")
+        );
+        square.coordinates.forEach((coor) =>
+          playerBoardSquares[coor].classList.add("selected")
+        );
+        board.forEach((object) => {
+          object.div.addEventListener("mouseenter", () => {
+            if (selectedShip && mouseIsDown) {
+              const isHorizontal =
+                Math.abs(
+                  selectedShip.coordinates[0] - selectedShip.coordinates[1]
+                ) === 1;
+              render.isHovering(
+                selectedShip.length,
+                object.index,
+                isHorizontal
+              );
+            }
+          });
+
+          object.div.addEventListener("mouseup", () => {
+            if (
+              !square.coordinates.includes(object.index) &&
+              !player.gameboard.board[object.index]
+            ) {
+              if (square.length === 1) {
+                playerBoard[object.index] = square;
+                playerBoard[square.coordinates[0]] = null;
+                square.coordinates[0] = object.index;
+                render.renderBoard(player);
+              }
+              if (square.length > 1) {
+                const isHorizontal =
+                  Math.abs(square.coordinates[0] - square.coordinates[1]) === 1;
+                const newCoords = [];
+                if (isHorizontal) {
+                  for (let i = 0; i < square.length; i++) {
+                    newCoords.push(object.index + i);
+                  }
+                } else {
+                  newCoords.push(object.index);
+                  for (let i = 0; i < square.length - 1; i++) {
+                    newCoords.push(newCoords[i] + 10);
+                  }
+                }
+                const valid = validate.validateDragShip(newCoords, playerBoard);
+                if (valid) {
+                  newCoords.forEach((coor) => (playerBoard[coor] = square));
+                  for (let i = 0; i < square.coordinates.length; i++) {
+                    playerBoard[square.coordinates[i]] = null;
+                    square.coordinates[i] = newCoords[i];
+                  }
+                  render.renderBoard(player);
+                } else return;
+              }
+            } else return;
+          });
+          document.addEventListener("mouseup", () => {
+            mouseIsDown = false;
+            selectedShip = null;
+            const playerBoardElement = document.querySelectorAll(
+              ".player-board .square"
+            );
+            playerBoardElement.forEach((el) => {
+              el.classList.remove("highlight");
+            });
+          });
+        });
+      }
+    });
+  };
+
+  return {
+    startGameEvent,
+    computerBoardEvents,
+    shuffleButtonEvents,
+    dragClickShips,
+  };
 })();
 
 export { events };
